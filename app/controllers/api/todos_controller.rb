7feb2ad2f66daacef1b6625924a044c9d576bdca
Tomate14-x <julioviana.2001.2018@gmail.com>
2022-07-26 17:11:35 -0300

    criando os dados dos todos com o faker 11:23

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,6 +1,7 @@
 class Api::TodosController < ApplicationController
   before_action :set_list
-  before_action :set_todo, only: [ :update, :show, :destroy]
+  before_action :set_todo, only: [:show, :update, :destroy]
+
   def index
     render json: @list.todos
   end
@@ -11,37 +12,36 @@ class Api::TodosController < ApplicationController
 
   def create
     @todo = @list.todos.new(todo_params)
-     
-    if @todo.save 
+    if @todo.save
       render json: @todo
     else
-      render json: {errors: @todo.errors},status: :unprocessable_entity
+      render json: { errors: @todo.errors }, status: :unprocessable_entity
     end
   end
 
   def update
     if @todo.update(todo_params)
-      render json: @todo
+      render json: @todo 
     else
-      render json: {errors: @todo.errors},status: :unprocessable_entity
+      render json: { errors: @todo.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
     @todo.destroy
-    render json:{message: "Todo destruido com sucesso"}
+    render json: { messsage: 'Todo deleted'}
   end
 
-  private
-    def todo_params
-      params.require(:todo).permit(:title,:complete,:rating,:price)
+  private 
+    def todo_params 
+      params.require(:todo).permit(:title, :complete, :rating, :price)
     end
 
-    def set_list
-      @list =List.find(params[:list_id])
+    def set_list 
+      @list = List.find(params[:list_id])
     end
 
-    def set_todo
+    def set_todo 
       @todo = @list.todos.find(params[:id])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,24 @@
 # List.create(title: 'sentimento',desc: 'Estou com muita fome')
 # List.create(title: 'linguagem',desc: 'gosto muito de React')
 # List.create(title: 'doce',desc: 'Pirulito do chaves')
+List.delete_all
+Todo.delete_all
+@nums = [ 1,2,3,4,5]
+
 10.times do
-  List.create(
+  @list = List.create(
     title: Faker::Color.color_name,
     desc: Faker::Hipster.sentence 
   )
+  3.times do
+    Todo.create(
+      title: Faker::Food.ingredient,
+      rating: @nums.sample ,
+      price: 1.2,
+      complete: Faker::Boolean.boolean,
+      list_id: @list.id
+    )
+  end
 end
 
 #rails db:seed
